d722e12d54b1c2da3d473520f2a14c6d665b96f3
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2023-10-20 21:33:11 +0600

    fixed extra padded blank issues

--- Tape.ts
+++ Tape.ts
@@ -28,24 +28,43 @@ class Tape {
       : this.tapeContent[this.tapeHead];
   }
 
-  write(char: string, direction: Direction) {
-    if (char.length != 1) throw 'Can only write a character in tape';
-    this.tapeContent[this.tapeHead] = char;
+  write(ch: string, direction: Direction) {
+    if (ch.length != 1) throw 'Can only write a character in tape';
+    this.tapeContent[this.tapeHead] = ch;
+
+    if (this.tapeHead == this.minIndex && ch == this.blankSymbol)
+      ++this.minIndex;
+    else if (this.tapeHead < this.minIndex && ch != this.blankSymbol)
+      this.minIndex = this.tapeHead;
+
+    if (this.tapeHead == this.maxIndex && ch == this.blankSymbol)
+      --this.maxIndex;
+    else if (this.tapeHead > this.maxIndex && ch != this.blankSymbol)
+      this.maxIndex = this.tapeHead;
 
     this.tapeHead += direction == Direction.RIGHT ? 1 : -1;
-    if (this.tapeHead < this.minIndex) this.minIndex = this.tapeHead;
-    if (this.tapeHead > this.maxIndex) this.maxIndex = this.tapeHead;
   }
 
   print() {
     let str: string = '';
     let cursrorLine: string = '';
+
+    for (let i = this.tapeHead; i < this.minIndex; ++i) {
+      str += this.blankSymbol;
+      cursrorLine += i == this.tapeHead ? '^' : ' ';
+    }
+
     for (let i = this.minIndex; i <= this.maxIndex; ++i) {
       str +=
         this.tapeContent[i] == undefined
           ? this.blankSymbol
           : this.tapeContent[i];
 
+      cursrorLine += i == this.tapeHead ? '^' : ' ';
+    }
+
+    for (let i = this.maxIndex + 1; i <= this.tapeHead; ++i) {
+      str += this.blankSymbol;
       cursrorLine += i == this.tapeHead ? '^' : ' ';
     }
 
