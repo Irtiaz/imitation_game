49f18c9be6a0560cf62d4dc793f206fcb2990e31
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2023-10-20 20:32:29 +0600

    added user interface in console to play around

--- index.ts
+++ index.ts
@@ -1,4 +1,36 @@
 import { Tape, Direction } from './Tape';
+import * as readline from 'readline/promises';
 
-const tape = new Tape('100010', 'B');
-tape.print();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function main() {
+  try {
+    const blankSymbol = await rl.question('Blank Symbol: ');
+    const intialTapeContents = await rl.question('Initial Tape Content: ');
+
+    const tape = new Tape(intialTapeContents, blankSymbol);
+
+    while (true) {
+      tape.print();
+
+      const command = await rl.question('');
+      if (command == '') break;
+      const replacer = command[0];
+      const directionString = command[1];
+
+      tape.write(
+        replacer,
+        directionString == '<' ? Direction.LEFT : Direction.RIGHT
+      );
+    }
+
+    rl.close();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main();
